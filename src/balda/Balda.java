44b8ffeb72aaf18e85f3d5ac489d0cc01ceb060b
matveev <matveevilya1998@mail.ru>
2024-02-03 17:19:25 +0300

    second commit after review from krios - the rules for naming variables and methods have been followed, the use of StringBuffer has been enabled instead of concatenation, and useless lines in the code have been removed.

--- src/balda/Balda.java
+++ src/balda/Balda.java
@@ -6,31 +6,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Balda {
-    private static String listOfWords[] = {"абзац", "порой", "закол", "ремис", "холоп"};
+    private static final String[] listOfWords = {"абзац", "порой", "закол", "ремис", "холоп"};
 
     private static final String regExpStart = "^(1|2)$";
     private static final String regExpGenrlCheck = "^[a-я]$";
+    private static final String failMessageAttempt = "Input Correct symbol (а-я)!";
+    private static final String failMessageOpinion = "Error! Input correct - \"1\" or \"2\"";
 
     public static void main(String[] args) {
         start();
-
     }
 
     private static void start() {
+        System.out.println("\nMenu \"Balda\"\nChoose you option ( \"1\" - start Game; \"2\" - quit game):");
 
+        String input = startCheck(regExpStart, failMessageOpinion);
 
-        System.out.println("Menu \"Balda\"\nChoose you option ( \"1\" - start Game; \"2\" - quit game):");
-        String str = "";
-        str = startCheck(regExpStart);
-        System.out.println(str);
-        switch (str) {
+        switch (input) {
             case "1": {
                 bigStart();
                 break;
             }
             case "2": {
                 System.out.println("quit game...");
-                System.out.println("Error");
                 break;
             }
         }
@@ -39,103 +37,81 @@ public class Balda {
 
     public static void bigStart() {
         int fail = 0;
+        int pos;
         boolean flag = true;
         Random random = new Random();
-        String generalWord = listOfWords[random.nextInt(5)];// example - малаша
-        String str = "", answer = "*****", temp = "";//!
-        for (int i = 0; i <= generalWord.length(); i++) {
-            str = str + "*";
-        }
+        String generalWord = listOfWords[random.nextInt(5)];
+        String secret = "";
 
-        System.out.println("..." + generalWord + "...");
-
+        secret = createSecret(secret, generalWord);
 
         while (flag) {
-            System.out.println("\nPlease print a letter( FAIL = " + fail + " | status =  " + answer + "):");
-            String letter = startCheck(regExpGenrlCheck);// case
+            System.out.println("\nPlease print a letter( FAIL = " + fail + " | status =  " + secret + "):");
 
-            temp = appearLetter(generalWord, letter);
+            String letter = startCheck(regExpGenrlCheck, failMessageAttempt).toLowerCase();
 
-            if (temp.equals("-1") | (generalWord.contains(letter) & !(answer.equals(temp)))) {
-                answer = appearLetter1(answer, temp);
+            pos = generalWord.indexOf(letter);
+
+            if (generalWord.contains(letter)) {
+                secret=appearingSecret(secret, generalWord, pos);
+
             } else {
                 System.out.println("\nWrong attempt, try again...");
-
                 fail++;
             }
-            if (answer.length() - answer.replace("*", "").length() == 0)
+            if (countingStar(secret) == 0)
                 flag = false;
             if (fail == 6)
                 flag = false;
         }
-        if (fail != 6)
-            System.out.println("\n Congratulations! your FAIL = " + fail + " | answer is  =  " + answer + "):\n");
+        if (countingStar(secret) == 0)
+            System.out.println("\nCongratulations! your FAIL = " + fail + " | answer is  =  " + secret + "):\n");
         else
-            System.out.println("You are lose! Secret word is " + answer);
+            System.out.println("You are lose! You has 6 attempts! Secret word is " + secret);
         start();
     }
 
-    private static String appearLetter1(String str1, String str2) {
-
-
-        int j = 0;
-        char[] mas2 = str2.toCharArray();
-        char[] mas1 = str1.toCharArray();
-        char c = '*';
-        for (int i = 0; i < mas2.length; i++) {
-            if (!(c == mas2[i]))
-                mas1[i] = mas2[i];
-
-        }
-        String temp = new String(mas1);
-
-        return temp;
+    private static int countingStar(String secret) {
+        return secret.length() - secret.replace("*", "").length();
     }
 
-    private static String appearLetter(String genWord, String letter) {
-        if (-1 == genWord.indexOf(letter))
-            return "";
+    private static String appearingSecret(String secret, String generalWord, int pos) {
+        char[] masSecret = secret.toCharArray();
+        char[] masGen = generalWord.toCharArray();
+        for (int i = 0; i < secret.length(); i++) {
+            if (masSecret[i] != masGen[i] & masGen[i] == secretSymdol(generalWord, pos))
+                masSecret[i] = generalWord.charAt(pos);
 
-
-        String answer = "", regexp = "";
-        int pos = 0, tempPos = genWord.indexOf(letter);
-
-        for (int i = 0; i <= tempPos; i++) {
-            regexp += ".";
-            pos = i;
         }
-
-        for (int i = 0; i < genWord.length(); i++) {
-            if (!(i == pos))
-                answer += "*";
-            else
-                answer += letter;
-        }
-        System.out.println("answer1 " + answer);
-        return answer;
+        return new String(masSecret);
     }
 
-
-    private static void general(String letter) {
-
+    private static String createSecret(String secret, String generalWorld) {
+        StringBuffer stringBuffer = new StringBuffer(secret);
+        for (int j = 0; j < generalWorld.length(); j++) {
+            stringBuffer.append("*");
+        }
+        return stringBuffer.toString();
     }
 
-    private static String startCheck(String regExp) {
-        String str = "";
+    private static char secretSymdol(String genWord, int pos) {
+        return genWord.charAt(pos);
+    }
+
+    private static String startCheck(String regExp, String message) {
         Scanner scanner = new Scanner(System.in);
-        Pattern pattern = Pattern.compile(regExp);// REGExp // simpler?
-        while (str.length() != 1) {
+        Pattern pattern = Pattern.compile(regExp);
+        while (true) {
             String inputFromUser = scanner.nextLine();
             Matcher matcher = pattern.matcher(inputFromUser);
             if (matcher.find()) {
-                str = matcher.group();
-                System.out.println("Correct input!");// put away?
-                return str;
+                System.out.println("Correct input!");
+                return matcher.group();
+
             } else {
-                str = "Error! Input correct - \"1\" or \"2\" ";// put away?
-                System.out.println(str);
+                System.out.println(message);
             }
         }
-        return str;
     }
 }
+
